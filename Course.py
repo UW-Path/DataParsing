bccf5f13c0c4d4cd35e2928357586421db98066a
Calder Lund <35615298+CalderLund@users.noreply.github.com>
2019-10-12 11:10:38 -0400

    Made patches to files

--- Course.py
+++ Course.py
@@ -1,20 +1,116 @@
+"""
+Course is an object class that stores information regarding a specific course.
+
+Date:
+October 11th, 2019
+
+Contributors:
+Calder Lund
+"""
 
 
-
-
-class Course():
-
+class Course:
     def __init__(self, html):
         self.html = html
-        self.code = html.find("a").get("name")
-        '''self.prereqs = self.__get_prereqs(html)
-        self.antireqs = self.__get_antireqs(html)
-        self.info = self.__get_info(html)
-        self.name = self.__get_name(html)
-        self.id = self.__get_id(html)'''
-        self.credit = self.__get_credit(html)
-        #self.when = self.__get_when(html)
-        self.online = any([isinstance(a.string, str) and a.string.endswith("Online") for a in html.find_all("a")])
+        self.code = self.__code()
+        self.prereqs = self.__prereqs()
+        self.antireqs = self.__antireqs()
+        self.info = self.__info()
+        self.name = self.__name()
+        self.id = self.__id()
+        self.credit = self.__credit()
+        self.online = self.__online()
 
-    def __get_credit(self, html):
-        print(html.find_all("b"))
+    def __code(self):
+        """
+        Returns the course code.
+
+        :return: string
+        """
+        # First occurrence always has course code
+        return self.html.find("a").get("name")
+
+    def __prereqs(self):
+        """
+        Returns a list of the course's required prerequisites.
+
+        :return: list(str)
+        """
+        all_i = self.html.find_all("i")
+        for i in all_i:
+            if i and i.string and i.string.strip().startswith("Prereq:"):
+                # TODO - Parse the prereqs string to return list of courses
+                return i.string.strip().replace("\n", " ")
+
+    def __antireqs(self):
+        """
+        Returns list of the course's anti-requisites.
+
+        :return: list(str)
+        """
+        all_i = self.html.find_all("i")
+        for i in all_i:
+            if i and i.string and i.string.strip().startswith("Antireq:"):
+                # TODO - Parse the antireqs string to return list of courses
+                return i.string.strip().replace("\n", " ")
+
+
+    def __info(self):
+        """
+        Returns the entire description of the course.
+
+        :return: string
+        """
+        return self.html.find_all("td")[3].string.strip("\n ")
+
+    def __name(self):
+        """
+        Returns the name of the course.
+
+        :return: string
+        """
+        # Course name is always second occurrence
+        return self.html.find_all("b")[1].string
+
+    def __id(self):
+        """
+        Returns the course ID.
+
+        :return: string
+        """
+        # self.html.find_all("td")[1] --> <td align="right">Course ID: XXXXXX</td>
+        return self.html.find_all("td")[1].string.strip("Course ID: ")
+
+    def __credit(self):
+        """
+        Returns the amount of credit received for taking this course.
+
+        :return: float
+        """
+        # Note:
+        # str(self.html.find("b")) --> "<b><a name = "CS 123"></a>CS 123 LEC 0.50</b>"
+        # str(self.html.find("b")).strip("</b>")[-4:] --> "0.50"
+        return float(str(self.html.find("b")).strip("</b>")[-4:])
+
+    def __online(self):
+        """
+        Returns whether the course is offered online.
+
+        :return: boolean
+        """
+        # The online indicator will only ever appear as the last occurrence of a
+        indicator = self.html.find_all("a")[-1]
+        return isinstance(indicator.string, str) and indicator.string.endswith("Online")
+
+    def __str__(self):
+        output = self.code + ": " + self.name
+        if self.online:
+            output += " (offered Online)"
+        output += "\n"
+        output += "\tCourse ID: " + self.id + "\tCourse credit: " + str(self.credit) + "\n"
+        output += "\t" + self.info + "\n"
+        if self.prereqs:
+            output += self.prereqs + "\n"
+        if self.antireqs:
+            output += self.antireqs + "\n"
+        return output
--- CourseParser.py
+++ CourseParser.py
@@ -1,26 +1,29 @@
-'''
-CourseParser.py is a library built to receive information on courses
-and their respected prerequisites.
+"""
+CourseParser.py is a library built to receive information on courses and
+their respected prerequisites.
 
 Date:
 October 10th, 2019
 
 Contributors:
 Calder Lund
-'''
+"""
 
 import urllib3
 from bs4 import BeautifulSoup
 from Course import Course
 
-class CourseParser():
+
+class CourseParser:
     '''
+    TODO - Document this portion of code
     CourseParser()
     '''
 
     def __init__(self):
         self.http = urllib3.PoolManager()
         self.data = None
+        self.courses = []
 
     def load_url(self, url):
         response = self.http.request('GET', url)
@@ -30,40 +33,10 @@ class CourseParser():
         html = open(file)
         self.data = BeautifulSoup(html, 'html.parser')
 
-    '''def get_courses(self):
-        courses = []
-        all = self.data.find_all('a')
-        for course in all:
-            code = course.get("name")
-            if code and not code.endswith("S"):
-                courses.append(course.get("name"))
-        return courses
-    '''
-    def get_courses(self):
-        courses = []
-        for c in self.info:
-            c.find_all("a")
-
-    def get_prereqs(self):
-        prereqs = []
-        all = self.data.find_all("i")
-        for a in all:
-            if a and a.string \
-               and a.string.startswith("Prereq:"):
-                prereqs.append(a.string)
-        return prereqs
-
     def get_course_info(self):
-        self.info = self.data.find_all("center")
-        self.courses = []
-        for info in self.info:
-            Course(info)
-
-
-
-    def __str__(self):
-        all = self.data.find_all("i")
-        return str(all)
+        info = self.data.find_all("center")
+        for i in info:
+            self.courses.append(Course(i))
         
 
 if __name__ == "__main__":
