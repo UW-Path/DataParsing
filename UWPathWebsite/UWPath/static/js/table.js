c46dcbb86852a4409da0312c91e0b4b72cc5b540
CalderLund <cjlund@uwaterloo.ca>
2020-01-15 11:07:23 -0500

    added functionality to accept 'CS 115, CS 135, CS 145'

--- UWPathWebsite/UWPath/static/js/table.js
+++ UWPathWebsite/UWPath/static/js/table.js
@@ -23,7 +23,7 @@ window.onload = function() {
       if (term === "trash") {
           emptyTrash();
       }
-      else {
+      else if (!(course.endsWith("*") || course.includes(","))) {
           /* This else statement is not implemented. It just passes through. */
           var list_of_courses_taken = getTaken(term);
           var current_term_courses = getCurrent(term);
@@ -34,13 +34,18 @@ window.onload = function() {
               data: {list_of_courses_taken: list_of_courses_taken, current_term_courses: current_term_courses},
               async: false,
               success: function(data) {
+                  debugger
                   var can_take = data.can_take;
+                  console.log(can_take);
                   // Do something
               },
               error: function(data) {
                   alert("Error: cannot determine if course can be taken.")
               }
           });
+      }
+      else {
+          // Do something when course is not a real course code.
       }
   });
 };
@@ -77,12 +82,18 @@ function emptyTrash() {
 function getTaken(term) {
     var taken = [];
     var arrayLength = terms.indexOf(term);
+    
     for (var i = 0; i < arrayLength; i++) {
-        debugger
         var term_courses = document.getElementById(terms[i]).getElementsByTagName("li");
+
         term_courses = Array.from(term_courses).map(function(c) {
-            return $(c).text().trim();
+            return $(c).text().trim().split(", ");
         });
+
+        // merges split arrays into one flattened array
+        term_courses = [].concat.apply([], term_courses);
+
+        // concat to taken
         taken = taken.concat(term_courses);
     }
     return taken;
@@ -91,7 +102,9 @@ function getTaken(term) {
 function getCurrent(term) {
     var term_courses = document.getElementById(term).getElementsByTagName("li");
     term_courses = Array.from(term_courses).map(function(c) {
-        return $(c).text().trim();
+        return $(c).text().trim().split(", ");
     });
+    // merges split arrays into one flattened array
+    term_courses = [].concat.apply([], term_courses);
     return term_courses;
 }
