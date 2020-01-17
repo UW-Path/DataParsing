2643641cd13d998b156f69f1a2505726a64d290b
owner <weihuang0313@gmail.com>
2020-01-17 02:53:01 -0500

    -Take out icon for table info -Fix bug where courses not validate when dragged back to "Required Column" or courses are dragged between school terms (within 1A,1B,2A,2B...)

--- UWPathWebsite/UWPath/static/js/table.js
+++ UWPathWebsite/UWPath/static/js/table.js
@@ -1,5 +1,5 @@
 // TODO - This will be dependent on how many terms the user specifies.
-const terms = ["1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B"];
+const terms = ["1A", "1B", "2A", "2B", "3A", "3B", "4A", "4B"]; //Add 5A 5B for Double Degrees
 
 /* Custom Dragula JS */
 window.onload = function() {
@@ -16,42 +16,51 @@ window.onload = function() {
     document.getElementById("5A"),
     document.getElementById("5B"),
     document.getElementById("trash")
-  ]).on('drop', function (el, target) {
+  ]).on('drop', function (el, target, source) {
       const term = $(target).attr("id");
+      const src = $(source).attr("id");
       let draggedId = el.id;
+      let termIndex = 0;
 
       if (term === "trash") {
-          let term_index = 0;
           emptyTrash();
-          checkSchedule(term_index)
+          checkSchedule(termIndex)
       }
       else if (term === "required"){
           // no operations if courses are dragged back to required courses
           // need to change in future because all course needs to be validated again
           document.getElementById(draggedId).style.color = "darkslateblue"; // back to original color
+          checkSchedule(termIndex)
       }
       else {
-          debugger
-          let term_index = terms.indexOf(term);
-          checkSchedule(term_index);
+          if (src === "required"){
+              //only check schedule for course after term has been dropped
+              termIndex = terms.indexOf(term);
+          }
+          else{
+              //src is Term Number
+              termIndex = Math.min(terms.indexOf(src), terms.indexOf(term));
+          }
+          // else needs to check all schedule again`
+          checkSchedule(termIndex);
       }
   });
 };
 
 
 function checkSchedule(term_index) {
-    var num_terms = terms.length;
-    var list_of_courses_taken = getTaken(term_index);
+    const numTerms = terms.length;
+    let listOfCoursesTaken = getTaken(term_index);
 
-    for (var i = term_index; i < num_terms; i++) {
-        var current_term_courses = getCurrent(i);
-        var current_term_courses_text = current_term_courses[1];
-        current_term_courses = current_term_courses[0];
-        var arrayLen = current_term_courses.length;
+    for (let i = term_index; i < numTerms; i++) {
+        let currentTermCourses = getCurrent(i);
+        const currentTermCoursesText = currentTermCourses[1];
+        currentTermCourses = currentTermCourses[0];
+        let arrayLen = currentTermCourses.length;
 
         if (arrayLen) {
-            for (var j = 0; j < arrayLen; j++) {
-                var course_li = current_term_courses[j];
+            for (let j = 0; j < arrayLen; j++) {
+                let course_li = currentTermCourses[j];
                 let course = course_li.innerText.trim();
 
                 if (!(course.endsWith("*") || course.includes(","))) {
@@ -60,31 +69,27 @@ function checkSchedule(term_index) {
                         url: 'http://127.0.0.1:8000/api/meets_prereqs/get/' + course,
                         type: 'get',
                         data: {
-                            list_of_courses_taken: list_of_courses_taken,
-                            current_term_courses: current_term_courses_text
+                            list_of_courses_taken: listOfCoursesTaken,
+                            current_term_courses: currentTermCoursesText
                         },
                         async: false,
                         success: function (data) {
-                            debugger
-                            var can_take = data.can_take;
-
+                            let canTake = data.can_take;
                             // Do something
-                            if (can_take) {
+                            if (canTake) {
                                 document.getElementById(draggedId).style.color = "green";
                             } else {
                                 document.getElementById(draggedId).style.color = "red";
                             }
                         },
                         error: function () {
-                            debugger
                             document.getElementById(draggedId).style.color = "grey";
                             // alert("Error: cannot determine if course can be taken.")
                         }
                     });
                 }
             }
-
-            list_of_courses_taken = list_of_courses_taken.concat(current_term_courses_text);
+            listOfCoursesTaken = listOfCoursesTaken.concat(currentTermCoursesText);
         }
     }
 }
@@ -93,7 +98,7 @@ function checkSchedule(term_index) {
 function addTask() {
     /* Get task text from input */
     const inputTask = document.getElementById("taskText").value.toUpperCase();
-    var id = inputTask + "(" + Math.round(Math.random() * 100 )  +")"; //generate random number to prevent unique id
+    let id = inputTask + "(" + Math.round(Math.random() * 100 )  +")"; //generate random number to prevent unique id
     id = id.replace(" ", "");
     // check if inputTask has whitespace
     if (/\S/.test(inputTask)) {
@@ -123,21 +128,21 @@ function emptyTrash() {
 
 
 function getTaken(term_index) {
-    var taken = [];
+    let taken = [];
 
-    for (var i = 0; i < term_index; i++) {
-        var term_courses = getCurrent(i)[1];
+    for (let i = 0; i < term_index; i++) {
+        let term_courses = getCurrent(i)[1];
         taken = taken.concat(term_courses);
     }
     return taken;
 }
 
 function getCurrent(term_index) {
-    var term_courses = document.getElementById(terms[term_index]).getElementsByTagName("li");
-    var term_courses_text = Array.from(term_courses).map(function(c) {
+    let termCourses = document.getElementById(terms[term_index]).getElementsByTagName("li");
+    let termCoursesText = Array.from(termCourses).map(function(c) {
         return $(c).text().trim().split(", ");
     });
     // merges split arrays into one flattened array
-    term_courses_text = [].concat.apply([], term_courses_text);
-    return [term_courses, term_courses_text];
+    termCoursesText = [].concat.apply([], termCoursesText);
+    return [termCourses, termCoursesText];
 }
