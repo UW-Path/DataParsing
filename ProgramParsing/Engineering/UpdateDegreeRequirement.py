4e6f864217a0d7d1c67fb0622870e931362ff48f
owner <weihuang0313@gmail.com>
2020-07-09 20:20:14 -0400

    Download engineering majors html

--- ProgramParsing/Engineering/UpdateDegreeRequirement.py
+++ ProgramParsing/Engineering/UpdateDegreeRequirement.py
@@ -18,16 +18,28 @@ def fetch_degree_req(path):
          return:
     """
 
-    pass
+    #fetch programs
+    http = urllib3.PoolManager()
+    response = http.request('GET', url_plans)
+    data = BeautifulSoup(response.data, 'html.parser')
 
-def fetch_faculty_minor(path):
-    """
-             path: str
-             Script to download all html pages for minors in Faculty of Math
-             return:
-    """
-    pass
+    Engineering = ["Architectural Engineering", "Biomedical Engineering", "Chemical Engineering", "Civil Engineering",
+                   "Computer Engineering", "Electrical Engineering", "Environmental Engineering", "Geological Engineering",
+                   "Management Engineering", "Mechanical Engineering", "Mechatronics Engineering", "Nanotechnology Engineering",
+                   "Software Engineering", "Systems Design Engineering"]
 
+    majors_links = data.find_all("a", class_="Level2Group")
+    for major in majors_links:
+
+        if (str(major.text) not in Engineering):
+            continue
+        print("Fetching {}...".format(str(major.text)))
+        href = root + major['href']
+        fileName = href.split("/")[-1]
+        fileName = "/" + fileName + ".html"
+        resp = get(href)
+        with open(path + fileName, 'wb') as fOut:
+            fOut.write(resp.content)
 
 if __name__ == '__main__':
     dir = os.path.dirname(__file__)
@@ -44,4 +56,3 @@ if __name__ == '__main__':
         os.mkdir(path)
 
     fetch_degree_req(path)
-    fetch_faculty_minor(path)
