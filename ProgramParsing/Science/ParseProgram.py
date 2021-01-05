277feaec9587de45cdd91b4b2c6fea885d93f5c9
Hao Wei <weihuang0313@gmail.com>
2021-01-04 20:19:02 -0500

    Address when a minimum of x unit isn't being parsed in SCIENCE faculty. Pickup lecture keyword as elective (need to watchout for new errors)

--- ProgramParsing/Science/ParseProgram.py
+++ ProgramParsing/Science/ParseProgram.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     files = set(["/Specs/" + f for f in os.listdir(path) if f.endswith(".html")])
 
     # Debugging purposes
-    # files = set(["/Specs/SCI-Honours-Earth-Sciences-Hydrogeology-Spec1.html"])
+    # files = set(["/Specs/SCI-Honours-Science5.html"])
     main(ScienceMajorParser, files, "Science")
 
