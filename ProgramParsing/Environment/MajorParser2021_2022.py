49161f29713ae7c8ffc26cd2210d1848f2b71e45
Brent <b828huang@rogers.com>
2021-09-06 21:40:38 -0400

    Finished Environment 2021-2022 parsing

--- ProgramParsing/Environment/MajorParser2021_2022.py
+++ ProgramParsing/Environment/MajorParser2021_2022.py
@@ -115,8 +115,7 @@ class EnvironmentMajorParser2021_2022(MajorParser):
                         hasCredit = 0.5
                     else:
                         hasCredit = float(hasCredit[0])
-                    print("here")
-                    print(line)
+
                     list = self._course_list(line)
                     if list:
                         self.requirement.append(
@@ -148,9 +147,6 @@ class EnvironmentMajorParser2021_2022(MajorParser):
 
         orCourse = re.findall(r"\b(?<!\/)[A-Z]{2,10}\b \b[0-9]{1,4}[A-Z]{0,1}\b or \b[A-Z]{2,10}\b \b[0-9]{1,4}[A-Z]{0,1}\b", line)
 
-        print(rangeCourse)
-        print(courses)
-        print(orCourse)
         if orCourse:
             # CS 135 or CS XXX in for 1hour and finally solved. Thanks a l
             for oC in orCourse:
